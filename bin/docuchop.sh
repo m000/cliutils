15be5eeb970acbf26897404ad7f6da4c3dd41e6f
Manolis Stamatogiannakis <mstamat@gmail.com>
2013-11-18 01:33:14 +0100

    Script seems to be working producing accurately trimmed seamless output. Have to do some testing to confirm.

--- bin/docuchop.sh
+++ bin/docuchop.sh
@@ -10,19 +10,27 @@ JQ="jq"
 RM="rm"
 PV="pv"
 PRINTF="printf"
+GREP="grep"
 
 ##################################################################
 # Runtime defaults
 ##################################################################
 # Black scene detection filter options.
-# 	d -> Detect black scenes of at least <d>sec
-#	pic_th -> A black frame conists of at least <pic_th>% black pixels.
-#	pix_th -> A black pixel may be up to <pix_th>% bright.
+#    d -> Detect black scenes of at least <d>sec
+#    pic_th -> A black frame conists of at least <pic_th>% black pixels.
+#    pix_th -> A black pixel may be up to <pix_th>% bright.
 # http://ffmpeg.org/ffmpeg-filters.html#blackdetect
 BD_OPTS=d=.2:pic_th=0.8:pix_th=0.10
 
 # Skip the content up to the first black scene. Useful for most documentaries.
 SKIP_FIRST=yes
+
+# Concat all videos in one before processing.
+# Uses more space but you won't have to care about videos not stopping on a black scene.
+CONCAT_ALL=yes
+
+# Extensions of files allowed to be processed. Make sure to include | delimiters on start and end!
+VALID_EXTENSIONS="|.avi|.mkv|.mp4|.m4v|"
 
 # For example, produce 8x8 PNG tiles of all keyframes (‘-skip_frame nokey’) in a movie:
 # ffmpeg -skip_frame nokey -i file.avi -vf 'scale=128:72,tile=8x8' -an -vsync 0 keyframes%03d.png
@@ -31,7 +39,7 @@ SKIP_FIRST=yes
 # Functions
 ##################################################################
 function analyze() {
-    # Analyzes input file and writes results to "$BD_CACHE", "$FR_CACHE" and "$ST_CACHE" files.
+    # Analyzes input file and writes results to "$bd_cache", "$fr_cache" and "$st_cache" files.
     # The first file contains the black detection analysis results in a custom text format.
     # The second file contains the frame analysis results in json format.
     # The third file contains stream information for the file.
@@ -39,6 +47,7 @@ function analyze() {
     # We duplicate the input file in two fifos.
     # This should allow for reading the file only once.
     local f="$1"
+    local caches="$2"
 
     # options used by black detection
     local bdopts_line="# BD_OPTS=$BD_OPTS"
@@ -48,20 +57,23 @@ function analyze() {
     local fifo_fr=/tmp/fifo_fr_$$
 
     # cache files to be created
-    BD_CACHE="$f".bd.cache
-    FR_CACHE="$f".fr.cache
-    ST_CACHE="$f".st.cache
+    local bd_cache="$f".bd.cache
+    local fr_cache="$f".fr.cache
+    local st_cache="$f".st.cache
 
     # always create streams files - this is cheap
-    $FFPROBE -show_format -show_streams -print_format json -v 9 "$f" > "$ST_CACHE"
+    $FFPROBE -show_format -show_streams -print_format json -v 9 "$f" > "$st_cache"
 
     # check if we have cached analysis files
-    if [ -f "$FR_CACHE" ] && [ -f "$BD_CACHE" ] && grep -q "$bdopts_line" "$BD_CACHE"; then
+    if [ -f "$fr_cache" ] && [ -f "$bd_cache" ] && grep -q "$bdopts_line" "$bd_cache"; then
         echo "Using cached analysis files for $f..."
+        echo "$bd_cache" > "$caches"
+        echo "$fr_cache" >> "$caches"
+        echo "$st_cache" >> "$caches"
         return
     fi
 
-    echo "Analyzing $f."
+    echo "Analyzing $f..."
 
     # create fifos and feed file into them
     mkfifo "$fifo_bd"
@@ -69,17 +81,22 @@ function analyze() {
     $PV -peb "$f" | tee "$fifo_bd" "$fifo_fr" >/dev/null &
 
     # run commands on fifos
-    $FFMPEG -i "$fifo_bd" -vf blackdetect="$BD_OPTS" -an -f null - 2> >(tr -u \\r \\n > "$BD_CACHE") &
-    $FFPROBE -select_streams v -show_frames -print_format json -v quiet "$fifo_fr" > "$FR_CACHE" &
+    $FFMPEG -i "$fifo_bd" -vf blackdetect="$BD_OPTS" -an -f null - 2> >(tr -u \\r \\n > "$bd_cache") &
+    $FFPROBE -select_streams v -show_frames -print_format json -v quiet "$fifo_fr" > "$fr_cache" &
     wait
 
     # Append black detection options signature and cleanup fifos.
-    echo "$bdopts_line" >> "$BD_CACHE"
+    echo "$bdopts_line" >> "$bd_cache"
     rm "$fifo_fr" "$fifo_bd"
+
+    # Write cache file names.
+    echo "$bd_cache" > "$caches"
+    echo "$fr_cache" >> "$caches"
+    echo "$st_cache" >> "$caches"
 }
 
 function prev_next_iframes() {
-    # parses "$FR_CACHE" and returns the position of the first iframes before and after the specified time
+    # parses "$fr_cache" and returns the position of the first iframes before and after the specified time
     local t_start="$1"
     local jqfilter='reduce (
         .frames[] |
@@ -94,7 +111,7 @@ function prev_next_iframes() {
         }
     ) | [.[]] | sort | .[]'
     # "$t_start" is used 3 times in the filter!
-    $JQ -r "$(printf "$jqfilter" "$t_start" "$t_start" "$t_start")" < "$FR_CACHE"
+    $JQ -r "$(printf "$jqfilter" "$t_start" "$t_start" "$t_start")" < "$fr_cache"
 }
 
 function video_bitrate() {
@@ -104,7 +121,7 @@ function video_bitrate() {
     #
     # Notes:
     #   In order to avoid warnings, we have to filter out frames without a pkt_dts_time.
-    #   In our experience this is only the last frame. 
+    #   In our experience this is only the last frame.
     #   The defined times should correspond to iframes, otherwise the calculation will be
     #   inaccurate if dt=O(sec).
     local t_start="$1"
@@ -124,7 +141,7 @@ function video_bitrate() {
             "size": (.size+$i["pkt_size"])
         }
     ) | .time as $t | .size | (8*(./$t))/1024'
-    local br=$($JQ -r "$(printf "$jqfilter" "$t_start" "$t_end")" < "$FR_CACHE")
+    local br=$($JQ -r "$(printf "$jqfilter" "$t_start" "$t_end")" < "$fr_cache")
 
     # round result to an int
     echo "$br/1" | bc
@@ -134,8 +151,8 @@ function encoding_options() {
     local brv="$1"
 
     # Dump info for debugging.
-    # $JQ '.streams[] | select(.codec_type == "audio")' < "$ST_CACHE"
-    # $JQ '.streams[] | select(.codec_type == "video")' < "$ST_CACHE"
+    # $JQ '.streams[] | select(.codec_type == "audio")' < "$st_cache"
+    # $JQ '.streams[] | select(.codec_type == "video")' < "$st_cache"
 
     # echo -n "-c:a copy "
     local jqfilter='.streams[] | select(.codec_type == "audio") |
@@ -160,80 +177,200 @@ function encoding_options() {
         end) as $custom |
         "-c:v \(.codec_name) -tag:v \(.codec_tag_string) -bf:v \(.has_b_frames) -level:v \(.level) \($custom) -b:v %s"
     '
-    $JQ -r "$(printf "$jqfilter" "$brv")" < "$ST_CACHE"
+    $JQ -r "$(printf "$jqfilter" "$brv")" < "$st_cache"
 
     # -c:v mpeg4 -tag:v XVID -profile:v 15  -preset slow "$outfile"</dev/null
     # level
 }
 
-for f in "$@"; do
-    [ -r "$f" ] || { echo "Skipping $f." 1>&2; continue; }
-    analyze "$f"
+function dt() {
+    # Subtracts timestamps and prints result in proper format for ffmpeg.
+    # ffmpeg requires having a zero before the floating point for -1<dt<1.
+    # E.g. .5 is invalid and should be printed as 0.5.
+    local dtr=$(printf '((%f)-(%f))\n' "$1" "$2" | bc -l)
+    printf "%f" "$dtr"
+}
 
-    # Reset scene counter.
-    sc=1
+function common_prefix() {
+    # Finds the longest common prefix of two strings.
+    # http://unix.stackexchange.com/a/18240/17259
+    local n=0
+    while [[ "${1:n:1}" != "" && "${1:n:1}" == "${2:n:1}" ]]; do
+       ((n++))
+    done
+    echo "${1:0:n}"
+}
+
+function has_valid_extension() {
+    local ext="${1##*.}"
+    echo "$VALID_EXTENSIONS" | $GREP -q "|\.$ext|"
+}
+
+function make_filename() {
+    # Creates a filename from the common prefixes of the supplied arguments.
+    local f=""
+    local prefix="$1"
+    local ext=""
+
+    for f in "$@"; do
+        # Initialize and test file extension.
+        has_valid_extension "$f" || { echo "Skipping '$f'. Bad extension." 1>&2; continue; }
+        [ "$ext" = "" ] && ext="${f##*.}"
+        [ "${f##*.}" = "$ext" ] || { echo "Skipping '$f'. Files must have a '.$ext' extension." 1>&2; continue; }
+
+        # Update prefix.
+        prefix=$(common_prefix "$f" "$prefix")
+    done
+
+    # Normalize prefix.
+    prefix="${prefix%%[*}"
+    prefix="${prefix%"${prefix##*[![:space:]]}"}"   #" # " is a syntax highlight fix
+    prefix="${prefix:=video$$}"
+
+    # Print results.
+    echo "$prefix"."$ext"
+}
+
+function video_concat() {
+    local f=""
+    local outfile="$1"
+    local cdemuxf="cdemuxf_$$_$RANDOM.txt"
+
+    # Throw away first argument which represents the outfile.
+    shift
+
+    # Test if the outfile is suitable for ffmpeg output.
+    has_valid_extension "$outfile" || { echo "Aborting. Bad extension for concat outfile '$f'." 1>&2; exit; }
+
+    # Prepare file for ffmpeg concat demuxer.
+    # http://ffmpeg.org/trac/ffmpeg/wiki/How%20to%20concatenate%20%28join,%20merge%29%20media%20files#demuxer
+    $RM -f "$cdemuxf"
+    for f in "$@"; do
+        [ -r "$f" ] || { echo "Skipping '$f'. Unreadable." 1>&2; continue; }
+        has_valid_extension "$f" || { echo "Skipping '$f'. Bad extension." 1>&2; continue; }
+        printf "file '%s'\n" "$f" >> "$cdemuxf"
+    done
+
+    # Concat & cleanup.
+    $FFMPEG -f concat -i "$cdemuxf" -c copy "$outfile" -loglevel warning </dev/null
+    $RM -f "$cdemuxf"
+}
+
+function video_chop() {
+    local f="$1"
+    local sc=1                                                  # scene counter
+
+    # Analyze file and get cached analysis files.
+    analyze "$f" "$f.meta.cache"
+    local bd_cache=$($GREP "\.bd\.cache$" "$f.meta.cache")      # black detect cache
+    local fr_cache=$($GREP "\.fr\.cache$" "$f.meta.cache")      # frames cache
+    local st_cache=$($GREP "\.st\.cache$" "$f.meta.cache")      # streams cache
+
+    # Variables used while looping scenes.
+    local black_start=""                                        # current black scene start
+    local black_end=""                                          # current black scene end
+    local black_start_prev=""                                   # previous black scene start
+    local black_end_prev=""                                     # previous black scene end
+    local l=""                                                  # temp variable
+    local t_start=""                                            # trim start time
+    local t_end=""                                              # trim end time
+    local outfile=""                                            # final scene video file
+    local outfile_1=""                                          # intermediate file 1 (reencoded)
+    local outfile_2=""                                          # intermediate file 2 (stream copy)
+    local reencode_bitrate=""
+    local reencode_options=""
 
     # Loop on scenes using the detected black scenes.
-    # Black scene line format:
-    # 	black_start:66.6667 black_end:66.967 black_duration:0.3003
-    # We transform each lines to shell variable assignments:
-    #   black_start=66.6667; black_end=66.967; black_duration=0.3003
-    grep ^black_start "$BD_CACHE" | sed 's/:/=/g; s/  */; /g' | while read l; do
-    	# copy old start/end values & assign new ones
-    	black_start_prev="$black_start"
-    	black_end_prev="$black_end"
-    	eval $l
+    # Black scene line format: black_start:66.6667 black_end:66.967 black_duration:0.3003
+    # We transform each lines to shell variable assignments: black_start=66.6667; black_end=66.967; black_duration=0.3003
+    grep ^black_start "$bd_cache" | sed 's/:/=/g; s/  */; /g' | while read l; do
+        # copy old start/end values & assign new ones
+        black_start_prev="$black_start"
+        black_end_prev="$black_end"
+        eval $l
 
-    	if [ "$black_start_prev" = "" ]; then
-	    	# first iteration processing
-    		[ "$SKIP_FIRST" = "yes" ] && continue
-    		t_start=0
-    		t_end="$black_start"
-    	else
-    		# normal processing
-    		t_start="$black_start_prev"
-    		t_end="$black_start"
-    	fi
+        if [ "$black_start_prev" = "" ]; then
+            # first iteration processing
+            [ "$SKIP_FIRST" = "yes" ] && continue
+            t_start=0
+            t_end="$black_start"
+        else
+            # normal processing
+            t_start="$black_start_prev"
+            t_end="$black_start"
+        fi
 
         # Construct outfiles & remove old outfiles.
-        outfile_1="${f%.*}_1.$((sc)).${f##*.}"
-        outfile_2="${f%.*}_2.$((sc)).${f##*.}"
-        outfile="${f%.*}.$((sc)).${f##*.}"
-		[ -f "$outfile" ] && $RM "$outfile"
+        outfile_1="${f%.*}.1.$((sc)).${f##*.}"
+        outfile_2="${f%.*}.2.$((sc)).${f##*.}"
+        outfile="${f%.*}_$((sc)).${f##*.}"
+        [ -f "$outfile" ] && $RM "$outfile"
         [ -f "$outfile_1" ] && $RM "$outfile_1"
         [ -f "$outfile_2" ] && $RM "$outfile_2"
 
-		echo "--------------------------------"
         echo "Processing scene $((sc++))..."
         # Find the iframes before and after the starting time.
         read prev_iframe next_iframe <<< $(prev_next_iframes "$t_start")
-
-        # Calculate times. Fix values<1 to start with 0 or ffmpeg will not be able to parse them.
-        t_middle="$next_iframe"
-        dt1=$(echo "dt=($t_middle-$t_start); if (dt>=0 && dt<1) {print 0}; print dt;" | bc -l)
-        dt2=$(echo "dt=($t_end-$t_middle); if (dt>=0 && dt<1) {print 0}; print dt;" | bc -l)
 
         # Calculate bitrate for the part that has to be reencoded.
         reencode_bitrate=$(video_bitrate "$prev_iframe" "$next_iframe")
 
         # Calculate ffmpeg options for the part that has to be reencoded.
         reencode_options=$(encoding_options "$reencode_bitrate"k)
-        # -c:a copy -c:v mpeg4 -tag:v XVID -profile:v 15 -level:v 5 -preset slow
 
-		# Process.
-        # Notes:
-        #   Use -ss as an input argument (before -i). Otherwise a few seconds may be lost from the begining of the video.
-        #   Always use /dev/null as the stdin of ffmpeg! Otherwise ffmpeg reads chars from stdin and gets confused.
-        echo "t_start=$t_start" "t_middle=$t_middle" "t_end=$t_end"
-        $FFMPEG -ss "$t_start"  -i "$f" -t "$dt1" $reencode_options "$outfile_1"</dev/null
-        $FFMPEG -ss "$t_middle" -i "$f" -t "$dt2" -c:a copy -c:v copy "$outfile_2"</dev/null
+        ##############################################################################################
+        # We want to trim video between the detected $t_start and $t_end and avoid reencoding as much
+        # as possible.
+        # In most modern video format this is not possible by simple stream copying, because of
+        # the use of iframes (also called keyframes). The video stream can only be seeked/trimmed on
+        # iframes.
+        #
+        # For this we create two separate trim files and then concatenate them:
+        #   1. The first video part is from $t_start until $next_iframe.
+        #      This part will usually be very short (O(sec)) and has to be reencoded.
+        #   2. The second video part is from $next_iframe until $t_end.
+        #      This is the bulk of the video and can be copied using simple stream copy.
+        #
+        # This is pictured in the following ascii art. We want to trim the part marked with #.
+        #
+        # ------------------------------------------------------------------------------------------
+        # |          |          |###########|######################|                               |
+        # ------------------------------------------------------------------------------------------
+        # |          |          |           |                      |                               |
+        # 0     prev_iframe  t_start   next_iframe               t_end                         video end
+        #
+        # Creating the first video still has a few catches. In order for the final result to be
+        # seamless, we need to employ the fast and accurate seek method described in the ffmpeg wiki:
+        # https://trac.ffmpeg.org/wiki/Seeking%20with%20FFmpeg
+        # This method does the seek to $t_start in two steps:
+        #   1. A fast seek to $prev_iframe applied on the input file (i.e. before -i argument).
+        #   2. A slow seek from that point until "$t_start" applied on the decoded stream (i.e. after -i argument).
+        #
+        # Note: On non-interactive invocations of ffmpeg stdin should be read from /dev/null!!!
+        #       Otherwise ffmpeg reads chars generated in the script, gets confused and fails.
+        ##############################################################################################
+        $FFMPEG -ss "$prev_iframe" -i "$f" -ss $(dt "$t_start" "$prev_iframe") -t $(dt "$next_iframe" "$t_start") $reencode_options "$outfile_1" -loglevel warning </dev/null
+        $FFMPEG -ss "$next_iframe" -i "$f" -t $(dt "$t_end" "$next_iframe") -c:a copy -c:v copy "$outfile_2" -loglevel warning </dev/null
 
-        # Use concat demuxer to join files.
-        # http://stackoverflow.com/a/15186625/277172
-        # http://ffmpeg.org/trac/ffmpeg/wiki/How%20to%20concatenate%20%28join,%20merge%29%20media%20files#demuxer
-        printf "file '%s'\nfile '%s'\n" "$outfile_1" "$outfile_2" > cc$$.txt
-        $FFMPEG -f concat -i cc$$.txt -c copy "$outfile"</dev/null
-        $RM cc$$.txt
-        exit
+        # Concat files. First argument is the final output.
+        video_concat "$outfile" "$outfile_1" "$outfile_2"
+        $RM -f "$outfile_1" "$outfile_2"
     done
-done
+}
+
+if [ "$CONCAT_ALL" = "yes" ]; then
+    concat_file="$(make_filename "$@")"
+    if [ -f "$concat_file" ]; then
+        echo "Continuing with existing concat file '$concat_file'..." 1>&2;
+    else
+        echo "Concatenating to '$concat_file'..." 1>&2;
+        video_concat "$concat_file" "$@"
+    fi
+    video_chop "$concat_file"
+else
+    for f in "$@"; do
+        [ -r "$f" ] || { echo "Skipping '$f'. Unreadable." 1>&2; continue; }
+        has_valid_extension "$f" || { echo "Skipping '$f'. Bad extension." 1>&2; continue; }
+        video_chop "$f"
+    done
+fi
