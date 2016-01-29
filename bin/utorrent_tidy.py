8f4923222aa6d446e3942184b53641161b7cf793
Manolis Stamatogiannakis <mstamat@gmail.com>
2016-01-29 18:53:31 +0100

    refactored print functions and guess save path function

--- bin/utorrent_tidy.py
+++ bin/utorrent_tidy.py
@@ -16,20 +16,72 @@ import os.path
 import sys
 import shutil
 import random
+import subprocess
 import tempfile
 from pprint import pprint
 import argparse
 
-MIN_SAVEPATH_LENGTH = 3
-SAVEPATH_SAMPLE_SIZE = 10
-SAVEPATH_SAMPLE_RETRIES = 5
-HR_LENGTH = 70
+
 
 try:
     import bencode
 except ImportError:
     print("Please install the bencode python module. E.g.: sudo pip install bencode", file=sys.stderr)
 
+###############################################################################
+#### Helpers ##################################################################
+###############################################################################
+def make_hr(text=None, width=None, fill='#'):
+    ''' Creates an ascii horizontal ruler.'''
+    if width is None:
+        try: width = int(subprocess.check_output(['stty', 'size']).split()[1])
+        except: width = 80
+    return ''.ljust(width, fill) if not text else ('%s %s ' % (3*fill, text)).ljust(width, fill)
+
+def print_hr(text=None, width=None, fill='#'):
+    ''' Print an ascii horizontal ruler.'''
+    print(make_hr(text, width, fill))
+
+def print_banner(lines=[], width=None, fill='#'):
+    ''' Prints the specified lines in a banner-like format.'''
+    ljust_len = max([len(s) for s in lines])+1
+    print_hr(width=width, fill=fill)
+    for l in lines:
+        print_hr(l.ljust(ljust_len), width=width, fill=fill)
+    print_hr(width=width, fill=fill)
+
+def guess_savepath(resume_dat):
+    ''' Attempt to guess the save path for uTorrent data.
+        This works as following:
+            - First the common prefix of all save locations is calculated.
+              If this is longer than MIN_SAVEPATH_LENGTH, it is returned.
+            - If not, this means that multiple save locations are in use.
+              Then, it is attempted to use sampling to guess the most popular
+              one. If sampling succeeds, the savepath is returned.
+            - Else, an exception is raised.
+    '''
+    MIN_SAVEPATH_LENGTH = 3
+    SAVEPATH_SAMPLE_SIZE = 10
+    SAVEPATH_SAMPLE_RETRIES = 5
+
+    allpaths = [ resume_dat[t]['path'] for t in resume_dat
+        if t.endswith('.torrent') and os.path.exists(resume_dat[t]['path'])
+    ]
+    savepath = os.path.commonprefix(allpaths)
+    if (len(savepath) < MIN_SAVEPATH_LENGTH):
+        # Failed to calculate savepath from all used paths. Try sampling.
+        for i in xrange(SAVEPATH_SAMPLE_RETRIES):
+            savepath = os.path.commonprefix( random.sample(allpaths, min(SAVEPATH_SAMPLE_SIZE, len(allpaths))) )
+            if (len(savepath) >= MIN_SAVEPATH_LENGTH):
+                break
+        if (len(savepath) < MIN_SAVEPATH_LENGTH):
+            raise Exception("Guessing of the uTorrent save path failed. You may want to retry in a few secs, or manually set the savepath.")
+    return savepath
+
+
+###############################################################################
+#### Real action ##############################################################
+###############################################################################
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='uTorrent tidy script. Moves completed files to directories matching their label. Writes a new resume data file.')
     parser.add_argument("-n", "--dry-run",
@@ -49,36 +101,25 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     ################################################
-    # Read resume data.
+    # Read resume data and calculate save path.
     ################################################
     with open(args.resumedat_in, 'rb') as dat_in:
         resume_dat = bencode.bdecode(dat_in.read())
 
-    print(HR_LENGTH*"*")
-    print("Dry Run: %s, Move Across FS: %s, Manual Savepath: %s" % (args.dryrun, args.xfs, args.savepath))
-    print(HR_LENGTH*"*")
-
-    ################################################
-    # Calculate the save path for the data.
-    ################################################
     if args.savepath:
         savepath = args.savepath
     else:
-        allpaths = [ resume_dat[t]['path'] for t in resume_dat
-            if t.endswith('.torrent') and os.path.exists(resume_dat[t]['path'])
-        ]
-        savepath = os.path.commonprefix(allpaths)
-        if (len(savepath) < MIN_SAVEPATH_LENGTH):
-            # Failed to calculate savepath from all used paths. Try sampling.
-            for i in xrange(SAVEPATH_SAMPLE_RETRIES):
-                savepath = os.path.commonprefix( random.sample(allpaths, min(SAVEPATH_SAMPLE_SIZE, len(allpaths))) )
-                if (len(savepath) >= MIN_SAVEPATH_LENGTH):
-                    break
-            if (len(savepath) < MIN_SAVEPATH_LENGTH):
-                print("Could not calculate a savepath using sampling.", file=sys.stderr)
-                print("You may want to retry in a few secs, or manually set the savepath.", file=sys.stderr)
-                sys.exit(1)
-    print("Savepath is: %s." % (savepath))
+        savepath = guess_savepath(resume_dat)
+
+    ################################################
+    # Print active configuration.
+    ################################################
+    cfg_banner = [ 
+            'Dry Run: %s' % (args.dryrun),
+            'Move Across FS: %s' % (args.xfs),
+            'Savepath: %s (%s)' % (savepath, 'guessed' if not args.savepath else 'user-set'),
+    ]
+    print_banner(cfg_banner)
 
     ################################################
     # Check what has to be done.
@@ -123,7 +164,7 @@ if __name__ == '__main__':
     ################################################
     # Execute actions.
     ################################################
-    print(HR_LENGTH*"-")
+    print_hr(fill='-')
     try:
         for torrent in actions:
             path_orig, path_dest, dir_dest = actions[torrent]
@@ -146,7 +187,7 @@ if __name__ == '__main__':
                 shutil.move(path_orig, dir_dest)
             resume_dat[torrent]['path'] = path_dest
             print("mv '%s' '%s'" % (path_orig, dir_dest))
-            print(HR_LENGTH*"-")
+            print_hr(fill='-')
     finally:
         ################################################
         # Write updated resume data.
