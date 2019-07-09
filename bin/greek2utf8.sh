b4b3f6bea8d848cf20f5695dc19db582aa562cac
Manolis Stamatogiannakis <mstamat@gmail.com>
2019-07-09 17:19:31 +0200

    Don't process suspected utf-8 files.

--- bin/greek2utf8.sh
+++ bin/greek2utf8.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 while (( "$#" )); do
+    detected=$(chardetect "$1" | awk -F': ' 'NR==1 {print $NF}')
+    if [ "${detected#*utf-8}" != "$detected" ]; then
+        printf "'%s' appears to be encoded in %s. Skipping.\n" "$1" "$detected" >&2
+        shift
+        continue
+    fi
+
     t=$(mktemp -t greek2utf8XXX)
     iconv -f iso8859-7 -t utf-8 < "$1" > "$t"
+    sed -i 's/’/Ά/g' "$t"
     mv "$t" "$1"
     shift
 done
