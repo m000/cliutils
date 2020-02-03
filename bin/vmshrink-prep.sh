bdf1915c76bf52ec781aa78ee5315109ab1951b5
Manolis Stamatogiannakis <mstamat@gmail.com>
2020-02-03 18:31:34 +0100

    vmshrink-prep.sh: Added rudimentary support for home directory cleanup.

--- bin/vmshrink-prep.sh
+++ bin/vmshrink-prep.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env zsh
+#
+# vmshrink-prep.sh
+#
+# A script to prepare virtual machines for shrinking.
+# This includes cleaning up various locations as well as recreating the
+# swap file. After running the script, shutdown the VM and use qemu-img
+# to create a shrinked copy of the image:
+# qemu-img convert -O qcow2 -o compat=0.10 prepared.qcow2 shrinked.qcow2
+#
 
 SCRIPT="$(basename "$0")"
 
@@ -10,6 +19,16 @@ function var_cleanup() {
     find /var -type f -name '*-old' -print -delete
 }
 
+function homedirs_cleanup() {
+    local flist=(.bash_history .cache .python_history .sudo_as_admin_successful .viminfo .ssh/authorized_keys)
+    for d in /root /home/*; do
+        echo "Cleaning up files in $d..."
+        for f in $flist; do
+            rm -rvf "$d/$f"
+        done
+    done
+}
+
 function swap_wipe() {
     local processed_swap=0
     swapon --show=NAME,UUID | tail -n +2 | while read dev uuid; do
@@ -17,16 +36,16 @@ function swap_wipe() {
         swapoff -U $uuid
         dd if=/dev/zero of="$dev" bs=8192
         mkswap -L "swap$processed_swap" -U $uuid $dev
-	swapon -U $uuid
+        swapon -U $uuid
         let processed_swap++
     done
 }
 
-zparseopts -D -E -a opts -- help swap var
+zparseopts -D -E -a opts -- help swap var homedirs
 
 if ((${opts[(I)-help]})); then
     echo "Script for preparing VM disks for shrinking/compacting."
-    echo "Usage: $SCRIPT [-help] [-var] [-swap] dir1 dir2 ..."
+    echo "Usage: $SCRIPT [-help] [-var] [-homedirs] [-swap] dir1 dir2 ..."
     exit 1
 fi
 
@@ -37,6 +56,10 @@ fi
 
 if ((${opts[(I)-var]})); then
     var_cleanup
+fi
+
+if ((${opts[(I)-homedirs]})); then
+    homedirs_cleanup
 fi
 
 if ((${opts[(I)-var]})); then
